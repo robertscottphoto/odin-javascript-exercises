1cc95fb980ed229a63b485b95e7e78362479f731
robertscottphoto <83343065+robertscottphoto@users.noreply.github.com>
2026-08-09 10:24:35 +0100

    complete 9.sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-    
+const sumAll = function(num1, num2) {
+    sum = 0
+    if(num1 > num2){
+        for(i = num2; i <= num1; i++){
+            sum += i
+        }
+    }
+    else if(num1 < num2){
+        for(i = num1; i <= num2; i++){
+            sum += i
+        }
+    }
+    return sum
 };
 
 // Do not edit below this line
