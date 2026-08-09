4bbb5ff774396be697c0b7be1f077bdb7618d76e
robertscottphoto <83343065+robertscottphoto@users.noreply.github.com>
2026-08-09 10:57:28 +0100

    completed 9 leap year task

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //year % 4 === 0 && year % 100 !== 0
+    if(year % 4 === 0){
+        if(year % 100 === 0 && year % 400 !== 0){
+            return "not a leap year"
+        }
+        else{
+            return "leap year"
+        }
+    }
 };
+
+/*Notes: 
+The solution takes the leap year categories and assigns each option to a variable which makes for better readability.  
+*/
+
+console.log(leapYears(2400))
 
 // Do not edit below this line
 module.exports = leapYears;
